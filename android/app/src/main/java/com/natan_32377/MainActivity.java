5c3f5cc3d5c1edd3b33e7bed98855313e7c7bd98
Crowdbotics <team@crowdbotics.com>
2021-12-14 00:33:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/natan_32377/MainActivity.java
@@ -0,0 +1,15 @@
+package com.natan_32377;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "natan_32377";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "natan_32377",
+  url: "https://natan_32377.botics.co",
+  api: "https://natan_32377.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
